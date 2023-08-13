d53fabe5af97fc9907a7b3d19204be671322ab9c
dagon1234 <dagontunk1234@gmail.com>
2023-08-13 13:04:38 +0700

    can run greet

--- greet-service/src/main/java/lab/microservice/greet/Application.java
+++ greet-service/src/main/java/lab/microservice/greet/Application.java
@@ -2,8 +2,10 @@ package lab.microservice.greet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class Application {
 
 	public static void main(String[] args) {
